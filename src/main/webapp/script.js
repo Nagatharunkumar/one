3fbe536daa7b31eaa8cb9f3b26bb573b68b5a531
Nagatharunkumar <108463075+Nagatharunkumar@users.noreply.github.com>
2025-04-22 09:57:14 +0530

    Update script.js

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -3,8 +3,14 @@ const products = {
     mobiles: [
         {
             id: 'm1',
-            title: 'iPhone 16 Pro',
-            price: 164900,
+            title: 'iPhone 15 Pro',
+            price: 134900,
+            image: 'https://m.media-amazon.com/images/I/81Os1SDWpcL._SL1500_.jpg'
+        },
+        {
+            id: 'm1',
+            title: 'iPhone 15 Pro',
+            price: 134900,
             image: 'https://m.media-amazon.com/images/I/81Os1SDWpcL._SL1500_.jpg'
         },
         {
